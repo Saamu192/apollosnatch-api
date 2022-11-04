cddeed2abdc8fab0ab449e05c926e12d7ba59793
Samuel Persuhn <samuelpr192@gmail.com>
2022-11-04 14:42:57 -0300

    fix: delay on browser

--- public/apollo/src/network.js
+++ public/apollo/src/network.js
@@ -13,6 +13,31 @@ class Network {
     const response = await fetch(url);
     return response.arrayBuffer();
   }
+
+  async getProperResolution(url) {
+    const startMs = Date.now();
+    const response = await fetch(url);
+    await response.arrayBuffer();
+    const endMs = Date.now();
+    const durationInMs = endMs - startMs;
+
+    const resolutions = [
+      { start: 10001, end: 20000, resolution: 144 },
+      { start: 5001, end: 10000, resolution: 360 },
+      { start: 0, end: 5000, resolution: 720 },
+    ];
+
+    const item = resolutions.find((item) => {
+      return item.start <= durationInMs && item.end >= durationInMs;
+    });
+
+    const LOWEST_RESOLUTION = 144;
+    if (!item) {
+      return LOWEST_RESOLUTION;
+    }
+
+    return item.resolution;
+  }
 }
 
 export { Network };
