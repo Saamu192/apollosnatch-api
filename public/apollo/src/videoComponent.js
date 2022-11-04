2cd18c97f2210af96e8cce6f1cb303e8496fa2f4
Samuel Persuhn <samuelpr192@gmail.com>
2022-11-04 14:56:02 -0300

    fix: exports

--- public/apollo/src/videoComponent.js
+++ public/apollo/src/videoComponent.js
@@ -1,45 +1,45 @@
 class VideoComponent {
-  constructor() {}
+  constructor() {
+    this.modal = {};
+  }
 
   initializePlayer() {
     const player = videojs("vid");
-    const modalDialog = videojs.getComponent("ModalDialog");
-    const modal = new modalDialog(player, {
+    const ModalDialog = videojs.getComponent("ModalDialog");
+    const modal = new ModalDialog(player, {
       temporary: false,
       closeable: true,
     });
+
     player.addChild(modal);
 
     player.on("play", () => modal.close());
 
     this.modal = modal;
   }
-
   configureModal(selected) {
     const modal = this.modal;
-    modal.on("modalopen", thus.getModalTemplate(selected, modal));
+    modal.on("modalopen", this.getModalTemplate(selected, modal));
     modal.open();
   }
 
   getModalTemplate(options, modal) {
     return (_) => {
       const [option1, option2] = options;
-
       const htmlTemplate = `
-      <div class='overlay'>
-        <div class='videoButtonWrapper'>
-          <button class='btn btn-dark' onclick='window.nextChunck('${option1}')'>
-          ${option1}
-          </button>
-          <button class='btn btn-dark' onclick='window.nextChunck('${option2}')'>
-          ${option2}
-          </button>
-        </div>
-      </div>`;
+          <div class='overlay'>
+              <div class='videoButtonWrapper'>
+                  <button class="btn btn-dark" onclick="window.nextChunk('${option1}')">
+                      ${option1}
+                  </button>
+                  <button class="btn btn-dark" onclick="window.nextChunk('${option2}')">
+                      ${option2}
+                  </button>
+              </div>
+          </div>
+          `;
 
       modal.contentEl().innerHTML = htmlTemplate;
     };
   }
 }
-
-export { VideoComponent };
